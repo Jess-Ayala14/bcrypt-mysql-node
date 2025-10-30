88a8c1c7c8aa83f57aff106d19bfa6f71607f245
Jess-Ayala14 <jessaydi@gmail.com>
2025-10-30 00:33:06 -0600

    1.01 commit

--- client/src/views/ini.js
+++ client/src/views/ini.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Inicio() {
   const [users, setUsuarios] = useState([]);
@@ -14,13 +15,11 @@ function Inicio() {
     <article className="content">
       <h1>Lista de Usuarios</h1>
       <p className='message'>Esta es la página de inicio, mostrando datos desde tu base de datos MySQL.</p>
-      {/*
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.fname} {user.email}</li>
-        ))}
-      </ul>
-      */}
+
+      <div class="content-action">
+        <Link to="/signup" class="btn singup">Registrarse</Link>
+        <Link to="/login" class="btn login">Iniciar Sesión</Link>
+      </div>
     </article>
   );
 }
